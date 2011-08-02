bee7624510cbb8a5a354155ed0e3b2552e53db98
Vegard <vegardjo@gmail.com>
2011-08-02 10:49:56 +0200

    various theme

--- themes/tedx/templates/node.tpl.php
+++ themes/tedx/templates/node.tpl.php
@@ -103,6 +103,16 @@
       print render($content);
     ?>
     
+        <!-- AddThis Button BEGIN -->
+        <div class="addthis_toolbox addthis_default_style" style="float: right">
+          <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+          <a class="addthis_button_tweet" tw:via="TEDxTromso"></a>
+          <a class="addthis_button_google_plusone"></a>
+        </div>
+        <script type="text/javascript">var addthis_config = {"data_track_clickback":true};</script>
+        <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4dad53b741234e4e"></script>
+        <!-- AddThis Button END -->
+    
     <?php
     // getting Full URL to use as Facebook Comment href
     global $base_url; 
